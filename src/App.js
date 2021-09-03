2e0349947e47342448b0f3184e3eb5b92e85fea5
Ayush Rameja <67544555+RamejaAyush@users.noreply.github.com>
2021-09-03 12:16:58 +0530

    added form section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles/App.css";
 import Nav from "./Components/Nav";
+import Form from "./Components/Form";
 
 function App() {
  return (
   <>
    <Nav />
+   <Form />
   </>
  );
 }
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,14 +2,21 @@ import "../styles/Nav.css";
 
 const Nav = () => {
  return (
-  <nav>
-   <h1>Todo App</h1>
-   <div className="navLinks">
-    <a href="#invalid">
-     <i class="fab fa-github"></i>
-    </a>
-   </div>
-  </nav>
+  <>
+   <nav>
+    <h1> Hi, there this is your Todo List</h1>
+    <div className="navLinks">
+     <a href="#invalid">
+      <i class="fab fa-github"></i>
+     </a>
+    </div>
+   </nav>
+   <img
+    src="https://images.pexels.com/photos/3831847/pexels-photo-3831847.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+    alt="Background"
+    id="background"
+   />
+  </>
  );
 };
 
